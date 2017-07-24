49d7a86eab968ca39a8ebbd94a2cb932225819a3
scott1541 <scott3933@gmail.com>
2017-07-24 16:28:45 +0100

    Reverse route works

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,16 +9,38 @@ class Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
-  def testpage = Action {
-    Ok(views.html.index("Hello World!"))
+  def testpage(input: String) = Action {
+    input match {
+      case "notfound" => NotFound
+      case "pagenotfound" => NotFound(<h1>Page Not Found</h1>)
+      case "badrequest" => BadRequest
+      case "oops" => InternalServerError
+      case _ => Ok(views.html.index("ALL OK!"))
+    }
+  }
+
+  def default(input: String) = Action {
+    Ok(views.html.test(input))
   }
 
   def list = Action {
     Ok(views.html.test("List"))
   }
 
-  def show = Action {
-    Ok(views.html.test("show"))
+  def show(id: Int) = Action {
+    Ok(views.html.test("ID number: " + id))
+  }
+
+  //def redirect = Action {
+    //Redirect("/test/all")
+  //}
+
+  def redirect = Action {
+    Redirect(routes.Application.plannedUpdates())
+  }
+
+  def plannedUpdates = Action {
+    Ok("Planned updates are in progress!")
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -8,6 +8,10 @@ scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq( jdbc , cache , ws   , specs2 % Test )
 
+// https://mvnrepository.com/artifact/org.scalastyle/scalastyle_2.10
+libraryDependencies += "org.scalastyle" % "scalastyle_2.10" % "0.9.0"
+
+
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
-resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"  
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
