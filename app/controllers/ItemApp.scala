ed30f5ae45cabfba6d9d68bbef0b4d464a1ccc60
scott1541 <scott3933@gmail.com>
2017-07-31 16:00:53 +0100

    File serve stuff

--- app/controllers/ItemApp.scala
+++ app/controllers/ItemApp.scala
@@ -5,6 +5,12 @@ import javax.inject.Inject
 import models.{Item, ItemDelete}
 import play.api._
 import play.api.mvc._
+import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.FileIO
+import akka.stream.scaladsl.StreamConverters
+
+import akka.util.ByteString
+import play.api.http.HttpEntity
 import play.api.i18n.{I18nSupport, MessagesApi}
 
 import scala.concurrent.Future
@@ -54,5 +60,46 @@ class ItemApp @Inject()(val messagesApi: MessagesApi) extends Controller with I1
       Item.items += item
       Redirect(routes.ItemApp.editItem)
     })
+  }
+
+  def fileRename = Action {
+    Ok.sendFile(
+      content = new java.io.File("C:/Users/Administrator/Documents//GitHub/ScalaPlay/public/temp/elliott.pdf"),
+      fileName = _ => "elot.pdf"
+    )
+  }
+
+  def fileServeCL = Action {
+    val file = new java.io.File("C:/Users/Administrator/Documents//GitHub/ScalaPlay/public/temp/elliott.pdf")
+    val path: java.nio.file.Path = file.toPath
+    val source: Source[ByteString, _] = FileIO.fromPath(path)
+
+    val contentLength = Some(file.length())
+
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Streamed(source, contentLength, Some("application/pdf"))
+    )
+  }
+
+  def fileAttachment = Action {
+    Ok.sendFile(
+      content = new java.io.File("C:/Users/Administrator/Documents//GitHub/ScalaPlay/public/temp/elliott.pdf"),
+      fileName = _ => "elot.pdf",
+      inline = false
+    )
+  }
+
+  def chunked = Action {
+    val data = getDataStream
+    val dataContent: Source[ByteString, _] =
+      StreamConverters.fromInputStream(() => data)
+    Ok.chunked(dataContent)
+  }
+
+
+  def chunkedFromSource = Action {
+    val source = Source.apply(List("kiki", "foo", "bar", "coo", "man", "boo"))
+    Ok.chunked(source)
   }
 }
