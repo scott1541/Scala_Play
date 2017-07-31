42ad692bcfbaaa74512a574ed00df0f18e092d2a
scott1541 <scott3933@gmail.com>
2017-07-31 16:09:38 +0100

    Fix stuff

--- app/controllers/ItemApp.scala
+++ app/controllers/ItemApp.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 import javax.inject.Inject
 
 import models.{Item, ItemDelete}
@@ -8,7 +9,6 @@ import play.api.mvc._
 import akka.stream.scaladsl.Source
 import akka.stream.scaladsl.FileIO
 import akka.stream.scaladsl.StreamConverters
-
 import akka.util.ByteString
 import play.api.http.HttpEntity
 import play.api.i18n.{I18nSupport, MessagesApi}
@@ -90,16 +90,31 @@ class ItemApp @Inject()(val messagesApi: MessagesApi) extends Controller with I1
     )
   }
 
-  def chunked = Action {
-    val data = getDataStream
-    val dataContent: Source[ByteString, _] =
-      StreamConverters.fromInputStream(() => data)
-    Ok.chunked(dataContent)
-  }
-
-
   def chunkedFromSource = Action {
     val source = Source.apply(List("kiki", "foo", "bar", "coo", "man", "boo"))
     Ok.chunked(source)
+  }
+
+  def chunked = Action {
+    val data =  new ByteArrayInputStream (serialise("Hello"))
+    println(serialise("hello")) // serializing something
+    println(deserialise(serialise("hello"))) // deserializing
+    val dataContent: Source[ByteString, _] = StreamConverters.fromInputStream(() => data)
+    Ok.chunked(dataContent)
+  }
+
+  def serialise(value: Any): Array[Byte] = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(stream)
+    oos.writeObject(value)
+    oos.close
+    stream.toByteArray
+  }
+
+  def deserialise(bytes: Array[Byte]): Any = {
+    val ois = new ObjectInputStream(new ByteArrayInputStream(bytes))
+    val value = ois.readObject
+    ois.close
+    value
   }
 }
